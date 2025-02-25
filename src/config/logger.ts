65cd9a6e13a14917bb1deb95f51ed2029657b6ee
AhmedAfzal2089 <ameafzal@gmail.com>
2025-02-25 23:52:09 +0500

    add logger

---
+++ src/config/logger.ts
@@ -0,0 +1,43 @@
+import winston from 'winston'
+import { Config } from '.'
+
+const logger = winston.createLogger({
+    level: 'info',
+    defaultMeta: {
+        serviceName: 'auth-service',
+    },
+    // where you want to store the log e.g database , files etc.
+    transports: [
+        new winston.transports.File({
+            level: 'info',
+            dirname: 'logs',
+            filename: 'combined.log',
+            format: winston.format.combine(
+                winston.format.timestamp(),
+                winston.format.json(),
+            ),
+            //this will not log on the terminal, if we are in testing dev then we silent the
+            silent: Config.NODE_ENV === 'test',
+        }),
+        new winston.transports.File({
+            level: 'error',
+            dirname: 'logs',
+            filename: 'error.log',
+            format: winston.format.combine(
+                winston.format.timestamp(),
+                winston.format.json(),
+            ),
+            silent: Config.NODE_ENV === 'test',
+        }),
+        new winston.transports.Console({
+            level: 'info',
+            format: winston.format.combine(
+                winston.format.timestamp(),
+                winston.format.json(),
+            ),
+            silent: Config.NODE_ENV === 'test',
+        }),
+    ],
+})
+
+export default logger
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,14 @@
 import app from './app'
 import { Config } from './config'
+import logger from './config/logger'
 
 const startServer = () => {
     const PORT = Config.PORT
     try {
-        app.listen(PORT, () => console.log(`listening on port ${PORT}`))
+        app.listen(PORT, () => {
+            logger.info('Server Listening on port, ', { port: PORT })
+            logger.info('Testing ....')
+        })
     } catch (err) {
         console.log(err)
         process.exit(1)
