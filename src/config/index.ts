8e1fa4b6acbe7edbdffcb42f9df174dad848d499
AhmedAfzal2089 <ameafzal@gmail.com>
2025-02-28 01:03:04 +0500

    setup database

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,9 +1,18 @@
 import { config } from 'dotenv'
-config()
+import path from 'path' // bulit in module in node js
 
-const { PORT, NODE_ENV } = process.env
+// this will use that env file according to the extension , ie. test , prod etc
+config({ path: path.join(__dirname, `../../.env.${process.env.NODE_ENV}`) })
+
+const { PORT, NODE_ENV, DB_HOST, DB_PORT, DB_USERNAME, DB_PASSWORD, DB_NAME } =
+    process.env
 
 export const Config = {
     PORT,
     NODE_ENV,
+    DB_HOST,
+    DB_PORT,
+    DB_USERNAME,
+    DB_PASSWORD,
+    DB_NAME,
 }
