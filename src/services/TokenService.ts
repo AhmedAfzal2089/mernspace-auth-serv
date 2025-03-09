48e2542adfb98ffe507338fb92c4102e3bdcf7cb
AhmedAfzal2089 <ameafzal@gmail.com>
2025-03-10 02:09:44 +0500

    refactor tokens

--- src/services/TokenService.ts
+++ src/services/TokenService.ts
@@ -1,10 +1,14 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import { JwtPayload, sign } from "jsonwebtoken";
 import fs from "fs";
 import path from "path";
+import { JwtPayload, sign } from "jsonwebtoken";
 import createHttpError from "http-errors";
 import { Config } from "../config";
+import { RefreshToken } from "../entity/RefreshToken";
+import { User } from "../entity/User";
+import { Repository } from "typeorm";
 export class TokenService {
+    constructor(private refreshTokenRepository: Repository<RefreshToken>) {}
     generateAccessToken(payload: JwtPayload) {
         let privateKey: Buffer;
         try {
@@ -34,5 +38,13 @@ export class TokenService {
             jwtid: String(payload.id), // embeding the refreshToken id of database to the refreshToken
         });
         return refreshToken;
+    }
+    async persistRefreshToken(user: User) {
+        const MS_IN_YEAR = 1000 * 60 * 60 * 24 * 365;
+        const newRefreshToken = await this.refreshTokenRepository.save({
+            user: user,
+            expiresAt: new Date(Date.now() + MS_IN_YEAR),
+        });
+        return newRefreshToken;
     }
 }
