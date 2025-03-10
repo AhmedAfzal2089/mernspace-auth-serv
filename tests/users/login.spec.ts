879b458d00505c85492b7e872a8e23a1ca586acd
AhmedAfzal2089 <ameafzal@gmail.com>
2025-03-11 00:46:17 +0500

    tests for login user

---
+++ tests/users/login.spec.ts
@@ -0,0 +1,123 @@
+import { DataSource } from "typeorm";
+import { AppDataSource } from "../../src/config/data-source";
+import app from "../../src/app";
+import request from "supertest";
+import { User } from "../../src/entity/User";
+import bcrypt from "bcrypt";
+import { Roles } from "../../src/constants";
+import { isJwt } from "../utils";
+import { App } from "supertest/types";
+
+describe("POST auth/login", () => {
+    let connection: DataSource;
+    beforeAll(async () => {
+        connection = await AppDataSource.initialize();
+    });
+    beforeEach(async () => {
+        await connection.dropDatabase();
+        await connection.synchronize();
+    });
+    afterAll(async () => {
+        await connection.destroy();
+    });
+    describe("Given all fields ", () => {
+        it("should return the 200 status code", async () => {
+            // Arrange
+            const userData = {
+                firstName: "Ahmed",
+                lastName: "Afzal",
+                email: "ahmedafzal2089@gmail.com",
+                password: "Password123!",
+            };
+
+            const hashedPassword = await bcrypt.hash(userData.password, 10);
+
+            const userRepository = connection.getRepository(User);
+            await userRepository.save({
+                ...userData,
+                password: hashedPassword,
+                role: Roles.CUSTOMER,
+            });
+
+            // Act
+            const response = await request(app as unknown as App)
+                .post("/auth/login")
+                .send({ email: userData.email, password: userData.password });
+            //Assert
+            expect(response.statusCode).toBe(200);
+        });
+        it("should return the access token and refresh token inside a cookie", async () => {
+            // Arrange
+            const userData = {
+                firstName: "Ahmed",
+                lastName: "Afzal",
+                email: "ahmedafzal2089@gmail.com",
+                password: "Password123!",
+            };
+
+            const hashedPassword = await bcrypt.hash(userData.password, 10);
+
+            const userRepository = connection.getRepository(User);
+            await userRepository.save({
+                ...userData,
+                password: hashedPassword,
+                role: Roles.CUSTOMER,
+            });
+
+            // Act
+            const response = await request(app as unknown as App)
+                .post("/auth/login")
+                .send({ email: userData.email, password: userData.password });
+
+            interface Headers {
+                ["set-cookie"]: string[];
+            }
+            // Assert
+            let accessToken = null;
+            let refreshToken = null;
+            const cookies =
+                (response.headers as unknown as Headers)["set-cookie"] || [];
+            cookies.forEach((cookie) => {
+                if (cookie.startsWith("accessToken=")) {
+                    accessToken = cookie.split(";")[0].split("=")[1];
+                }
+
+                if (cookie.startsWith("refreshToken=")) {
+                    refreshToken = cookie.split(";")[0].split("=")[1];
+                }
+            });
+            expect(accessToken).not.toBeNull();
+            expect(refreshToken).not.toBeNull();
+
+            expect(isJwt(accessToken)).toBeTruthy();
+            expect(isJwt(refreshToken)).toBeTruthy();
+        });
+        it("should return the 400 if email or password is wrong", async () => {
+            // Arrange
+            const userData = {
+                firstName: "Rakesh",
+                lastName: "K",
+                email: "rakesh@mern.space",
+                password: "password",
+            };
+
+            const hashedPassword = await bcrypt.hash(userData.password, 10);
+
+            const userRepository = connection.getRepository(User);
+            await userRepository.save({
+                ...userData,
+                password: hashedPassword,
+                role: Roles.CUSTOMER,
+            });
+
+            // Act
+            const response = await request(app as unknown as App)
+                .post("/auth/login")
+                .send({ email: userData.email, password: "wrongPassword" });
+
+            // Assert
+
+            expect(response.statusCode).toBe(400);
+        });
+    });
+});
