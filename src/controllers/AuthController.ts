5f32b3a8908ec7a9d069988573c5dab29688b8b0
AhmedAfzal2089 <ameafzal@gmail.com>
2025-03-11 00:09:42 +0500

    change statuscode of login endpoint

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -155,7 +155,7 @@ export class AuthController {
                 httpOnly: true, // Very important
             });
             this.logger.info({ id: user.id });
-            res.status(201).json({ id: user.id });
+            res.json({ id: user.id });
         } catch (err) {
             next(err);
             return;
