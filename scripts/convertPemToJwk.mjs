05614588bbf72ef317a0d256afe4e776e3c1665b
AhmedAfzal2089 <ameafzal@gmail.com>
2025-03-12 02:05:40 +0500

    ignore public folder

--- scripts/convertPemToJwk.mjs
+++ scripts/convertPemToJwk.mjs
@@ -1,12 +1,11 @@
-import fs from 'fs'
-import rsaPemToJwk from 'rsa-pem-to-jwk'
+import fs from "fs";
+import rsaPemToJwk from "rsa-pem-to-jwk";
 
-const privateKey = fs.readFileSync('./certs/private.pem')
+const privateKey = fs.readFileSync("./certs/private.pem");
 
 // sig means jwk will be used only for to verify the signature
-const jwk = rsaPemToJwk(privateKey, { use: 'sig' }, "public")
+const jwk = rsaPemToJwk(privateKey, { use: "sig" }, "public");
 
-
-console.log(JSON.stringify(jwk))
+console.log(JSON.stringify(jwk));
 
 // run the file manually through node, so it will show the console
